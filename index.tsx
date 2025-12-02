63c50189548c33adcfec4ef126824ce725a0cb19
Harshil Patel <hpatel@geturgently.com>
2025-12-02 17:32:19 -0500

    Fix deployment issue

--- index.tsx
+++ index.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Buffer } from 'buffer';
+
+if (typeof globalThis.Buffer === 'undefined') {
+    globalThis.Buffer = Buffer;
+}
+
+if (typeof globalThis.process === 'undefined') {
+    globalThis.process = {
+        env: {},
+    } as any;
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
